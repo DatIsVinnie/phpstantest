b27121efc6e84c82d127418db7d87a459459cbb3
DatIsVinnie <57494611+DatIsVinnie@users.noreply.github.com>
2024-05-28 16:01:17 +0200

    t

--- root/index.php
+++ root/index.php
@@ -5,20 +5,12 @@
 $naam = "John Doe";
 
 // Proberen de waarde van een niet-gedefinieerde variabele op te halen
-echo "Hallo, mijn naam is $name";
-
 
 // Een array met enkele gegevens
 $data = [
     'name' => 'John Doe',
     'email' => 'john@example.com'
 ];
-
-// Proberen om de waarde van een niet-bestaande index 'age' op te halen
-$age = $data['age'];
-
-// Deze lijn zal een undefined index-waarschuwing veroorzaken omdat 'age' niet bestaat in de $data array
-echo "Age: $age";
 
 
 
--- root/test.php
+++ root/test.php
@@ -2,10 +2,5 @@
 
 
 // Proberen om de waarde van een niet-bestaande index 'age' op te halen
-$age = $data['age'];
 
 // Deze lijn zal een undefined index-waarschuwing veroorzaken omdat 'age' niet bestaat in de $data array
-echo "Age: $age";
-echo "Age: $age";
-echo "Age: $age";
-echo "Age: $age";
